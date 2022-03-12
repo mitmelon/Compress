dedb89ddc3673eb5ff64af8c71f99d0a70e3320f
Manomite <manomitehq@gmail.com>
2022-03-12 17:00:32 +0100

    New Update Change Logs

--- Compress/Compress.php
+++ Compress/Compress.php
@@ -1,14 +1,33 @@
 <?php
 namespace Compress;
 /**
- * Compresses files including zip, image, pdf and lots more
+ * Compresses and encrypt files including zip, image, pdf and lots more
  *
-  * @link https://manomiteblog.com for Programming Tutorials
- *  @author Manomite <manomitehq@gmail.com>
- *  @version 1.0.1
+ * @link https://blog.manomite.net for Programming Tutorials
+ * @author Manomite <manomitehq@gmail.com>
+ * @version 2.0.0
  */
 class Compress
 {
+    /**
+     * Encryption block chunk size
+     *
+     * @param Int $block
+     */
+     public int $block = 1024;
+     /**
+     * Encryption cipher
+     *
+     * @default aes-256-cbc
+     */
+     public $cipher = 'aes-256-cbc';
+     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     //Encryption file temporary storage.                                                                       //
+     //Do not touch here please.                                                                               //
+     private $temp = __DIR__.'/compress_tmp_file';                                                            //
+     private $dest_temp = __DIR__.'/dest_compress_tmp_file';                                                 //
+     private $encrypt_key = null;                                                                           //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
      /**
      * Text compress
      *
@@ -32,25 +51,44 @@ class Compress
      *
      * @param String $filePath
      * @param String $storePath
-     * @param Bolean $removeMeta
+     * @param Array $options ["removeMeta" => false, "encrypt" => false, "key" => "password"]
      */
-    public static function compressFile(String $filePath, String $storePath, $removeMeta = false)
+    public static function compressFile(String $filePath, String $storePath, array $options = [])
     {
-       
         try {
             $file = @fopen($filePath, "rb");
             $content = fread($file, filesize($filePath));
-             //Meta heads are removed in new version which cannot be reversed
-            if ($removeMeta) {
-                $replace    = '@<meta[^>]*?>';
-                $with       = ' ';
-                $content    = str_replace($replace, $with, $content);
-                $content    = preg_replace("/$replace/", $with, $content);
+            //Check if option is not empty
+            if (!empty($options)) {
+                //Meta heads are removed in new version which cannot be reversed
+                $isMeta = isset($options["removeMeta"]) ? ($options["removeMeta"] === true ? true : false) : false;
+                if ($isMeta) {
+                    $replace    = '@<meta[^>]*?>';
+                    $with       = ' ';
+                    $content    = str_replace($replace, $with, $content);
+                    $content    = preg_replace("/$replace/", $with, $content);
+                }
+                //Check if encryption is set
+                $isEncrypt = isset($options["encrypt"]) ? ($options["encrypt"] === true ? true : false) : false;
+                if ($isEncrypt) {
+                    //mt_rand() was used to avoid file collition among many users compressing at the same time.
+                    $t = $this->temp.'_'.mt_rand().'.encrypt';
+                    $d = $this->dest_temp.'_'.mt_rand().'.encrypt';
+                    file_put_contents($t, $content);
+                    $key = isset($options["key"]) ? $options["key"] : null;
+                    $encrypt_key = $this->encryptFile($t, $d, $key);
+                    $file_s = @fopen($d, "rb");
+                    $content = fread($file_s, filesize($d));
+                    fclose($file_s);
+                    unlink($t);
+                    unlink($d);
+                }
             }
-            $content    = self::compressor(serialize($content));
+            $content = $this->compressor(serialize($content));
             fclose($file);
             file_put_contents($storePath, $content);
-            return true;
+            //Make sure you store your encryption key for this file if you used the encryption option
+            return $encrypt_key;
         } catch (\Extension $e) {
             return $e->getMessage();
         }
@@ -60,16 +98,98 @@ class Compress
      *
      * @param String $fileInputPath
      * @param String $fileOutputPath
+     * @param String $encrypt_key
      */
-    public static function uncompressFile($fileInputPath, $fileOutputPath)
+    public static function uncompressFile($fileInputPath, $fileOutputPath, $encrypt_key = null)
     {
         try {
             $file = fopen($fileInputPath, "rb");
-            $contents = unserialize(self::uncompressor(fread($file, filesize($fileInputPath))));
+            $contents = unserialize($this->uncompressor(fread($file, filesize($fileInputPath))));
+            //Check if key is given; File might be encrypted
+            if(!empty($encrypt_key)){
+                $t = $this->temp.'_'.mt_rand().'.decrypt';
+                $d = $this->dest_temp.'_'.mt_rand().'.decrypt';
+                file_put_contents($t, $contents);
+                $this->decryptFile($t, $d, $encrypt_key);
+                $file_s = @fopen($d, "rb");
+                $contents = fread($file_s, filesize($d));
+                fclose($file_s);
+                unlink($t);
+                unlink($d);
+            }
             file_put_contents($fileOutputPath, $contents);
             fclose($file);
             return true;
         } catch (\Extension $e) {
+            return $e->getMessage();
+        }
+    }
+    /**
+     * @param  $source  Path of the unencrypted file
+     * @param  $dest  Path of the encrypted file to created
+     * @param  $key  Encryption key
+     */
+    private function encryptFile($source, $dest, $key = null)
+    {
+        try {
+            if (!extension_loaded('openssl')) {
+                return "Sorry! openssl extension is not installed on your system. Please install it using sudo apt-get install openssl";
+            }
+
+            if (empty($key)) {
+                $key = openssl_random_pseudo_bytes(120);
+            }
+
+            $ivLenght = openssl_cipher_iv_length($this->cipher);
+            $iv = openssl_random_pseudo_bytes($ivLenght);
+
+            $fpSource = fopen($source, 'rb');
+            $fpDest = fopen($dest, 'w');
+
+            fwrite($fpDest, $iv);
+
+            while (! feof($fpSource)) {
+                $plaintext = fread($fpSource, $ivLenght * $this->block);
+                $ciphertext = openssl_encrypt($plaintext, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
+                $iv = substr($ciphertext, 0, $ivLenght);
+
+                fwrite($fpDest, $ciphertext);
+            }
+
+            fclose($fpSource);
+            fclose($fpDest);
+            return $key;
+        } catch(\Exception $e){
+            return $e->getMessage();
+        }
+    }
+    /**
+     * @param  $source  Path of the encrypted file
+     * @param  $dest  Path of the decrypted file
+     * @param  $key  Encryption key
+     */
+    private function decryptFile($source, $dest, $key)
+    {
+        try {
+            $ivLenght = openssl_cipher_iv_length($this->cipher);
+
+            $fpSource = fopen($source, 'rb');
+            $fpDest = fopen($dest, 'w');
+
+            $iv = fread($fpSource, $ivLenght);
+
+            while (! feof($fpSource)) {
+                $ciphertext = fread($fpSource, $ivLenght * ($this->block + 1));
+                $plaintext = openssl_decrypt($ciphertext, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
+                $iv = substr($plaintext, 0, $ivLenght);
+
+                fwrite($fpDest, $plaintext);
+            }
+
+            fclose($fpSource);
+            fclose($fpDest);
+            return true;
+        } catch(\Exception $e){
             return $e->getMessage();
         }
     }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Compress
 [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fmitmelon%2FCompress.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2Fmitmelon%2FCompress?ref=badge_shield)
 
- Advanced file compresser which reduces large files to smaller chunks by saving them as binary for later use. Many applications supports file uploads which consumes large memory sizes on their systems. This library has been designed to reduce both zip, rar, images, pdf, words sizes or any kinds of files or documents into smaller pieces.
+ Advanced file compresser which reduces and encrypts large files to smaller chunks by saving them as binary for later use. Many applications supports file uploads which consumes large memory sizes on their systems. This library has been designed to reduce both zip, rar, images, pdf, words sizes or any kinds of files or documents into smaller pieces.
 
 ## Install:
 Use composer to install
@@ -18,32 +18,32 @@ require_once __DIR__."/vendor/autoload.php";
 $compress = new Compress\Compress;
 
 /**
- * @param $filePath string
+ * @param String $filePath
  * File location to be compressed
- *
- * @param $storePath string
+ * @param String $storePath
  * Path to output compressed binary file to
- *
+ * @param Array optional $options ["removeMeta" => false, "encrypt" => false, "key" => "password"]
+ * Options to remove meta and encrypt file
  */
-$compress::compressFile($filePath, $storePath, $removeMeta);
+$compress::compressFile($filePath, $storePath, $options = []);
 
 //Compress Image file
-$compress::compressFile(__DIR__.'/image.png', __DIR__.'/image.txt', true));
+$compress::compressFile(__DIR__.'/image.png', __DIR__.'/image.txt'));
 
 // Compress PDF
 $compress::compressFile(__DIR__.'/file.pdf', __DIR__.'/file.txt', true));
-//Compress as lot files you want including zip files
+//Compress as lot of files you want including zip files
 
 /**
  * UnCompress Image file [Get original file back from stored binary]
- * @param $storePath string
+ * @param String $storePath
  * Path containing binary file which was compressed
- *
- * @param $fileOutputPath string
+ * @param String $fileOutputPath
  * Path to output original file to
- *
+ * @param String $encrypt_key
+ * If your file was encrypted then provide the key for decryption as third argument
  */
-$compress::uncompressFile($storePath, $fileOutputPath);
+$compress::uncompressFile($storePath, $fileOutputPath, $encrypt_key = null);
 
 //Uncompress Image
 $compress::uncompressFile(__DIR__.'/image.txt', __DIR__.'/image.png');
@@ -53,9 +53,38 @@ $compress::uncompressFile( __DIR__.'/file.txt', __DIR__.'/file.pdf')
 
 ```
 
+
+# Changelog
+
+All notable changes to this project will be documented here.
+
+## [2.0.0] - 2022-03-12
+
+We're super excited to announce `encryption` version of compress
+
+When you upgrade, consider updating compressFile() parameter. The third argument is now array
+
+### New features
+
+  - 🌟 Added AES 256 CBC Mode encryption
+
 # Future Update
 
-Adding binary file encryptions for security purposes.
+* Store files on centralize and decentralize cloud providers
+* Add key vaults to safely store your file keys
+* Add file signing digest key using SHA-3 Algorithmn
+* Add asymmetric file encryption protocol with Diffie Helman Key Exchange
+
+# Support
+
+If you love my project and wish to assist me to keep working on this project. Below is my Wallet Address.
+
+BTC Wallet : 14PAtDFHTwH6SWdhGXRHGWDWYWDgVfHr6R
+ETH Wallet : 0x8c26549052667A0b77327505D2Ea1fe4c207630e
+
+If you donated, please send me a mail at manomitehq@gmail.com with your name so I can list you here among my supporters
+I will really appreciate your donations. Thanks for using Compress.
+
 
 # License
 
